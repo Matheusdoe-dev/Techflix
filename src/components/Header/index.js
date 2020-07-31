9b2c9b9da7230e2af94db33b25e5d9b020ac77d0
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-31 21:07:25 +0000

    :lipstick: feat: Header component created

---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+// components
+import { Container, Grid } from "@material-ui/core";
+import { Button } from "../../styles/objects/button";
+import { Link, useHistory } from "react-router-dom";
+import { HeaderContainer } from "./styles";
+// styled-components
+import { Brand } from "../../styles/components/brand";
+// imgs
+import logoImg from "../../assets/icons/techflix.svg";
+
+const Header = () => {
+  const history = useHistory();
+
+  const handleEntrar = (event) => {
+    event.preventDefault();
+    history.push("/index");
+  };
+
+  return (
+    <HeaderContainer>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <Link to="/index">
+              <Brand src={logoImg} alt="Techlix" />
+            </Link>
+          </Grid>
+          <Grid item container xs={12} sm={8} justify="flex-end">
+            <Button variant="contained" color="primary" onClick={handleEntrar}>
+              Entrar
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  & {
+    padding: var(--gap) 0;
+  }
+`;
