fd3fe5fc4edcb8586570e631d34b5f8c2bcf702f
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-08-01 20:28:35 +0000

    :construction: chore: Added Index page route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 // components
 import Home from "./pages/Home/index";
+import Index from "./pages/Index/index";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/index" component={Index} />
       </Switch>
     </BrowserRouter>
   );
